3f82597cb33b1599af13b79a184af977a715a3a9
PrabhatRanjan729Divastra <83538272+PrabhatRanjan729Divastra@users.noreply.github.com>
2021-05-02 20:58:31 +0530

    Add files via upload

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,33 +1,3 @@
-async function signup(e){
-					e.preventDefault();
-					const email=document.querySelector("#signUpEmail");
-					const password=document.querySelector("#signUpPassword");
-					
-					try{
-						const result = await firebase.auth().createUserWithEmailAndPassword(email.value, password.value);
-						console.log(result);
-					}
-					catch(error){
-						console.log(error);
-					}
-}
-
-async function login(e){
-					e.preventDefault();
-					const email=document.querySelector("#logInEmail");
-					const password=document.querySelector("#logInPassword");
-					
-					try{
-						const result = await firebase.auth().signInWithEmailAndPassword(email.value, password.value);
-						console.log(result);
-						alert("logged in");
-					}
-					catch(error){
-						console.log(error);
-						alert(error.message);
-					}
-}
-
 function logout(){
 					firebase.auth().signOut();
 }
@@ -36,8 +6,7 @@ firebase.auth().onAuthStateChanged((user) => {
 				  if (user) {
 				    // User is signed in, see docs for a list of available properties
 				    // https://firebase.google.com/docs/reference/js/firebase.User
-				    console.log(user[0]);
-				   alert(user);
+				    console.log(user);
 				    // ...
 				  } else {
 				    alert("Signed Out");
